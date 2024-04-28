22db4792eed0dea26399572fe35edb3f66fe9229
Agustin Chiarotto <agusgc_99@hotmail.com>
2024-04-28 19:10:48 -0300

    correcciones en list component

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,18 +1,36 @@
-import Task from '../Task/Task'
+import Task from "../Task/Task";
 
-const List = ({filterTasks, cantTasks, onClickHandlerDelete, onClickHandlerCheck}) => {
+const List = ({
+  filterTasks,
+  cantTasks,
+  onClickHandlerDelete,
+  onClickHandlerCheck,
+}) => {
+  return (
+    <div>
+      {/* Si aca usan un ul, deberian usar un li dentro de Task o como wrapper de Task */}
+      <ul>
+        {filterTasks.length > 0 ? (
+          <p>
+            Tenes en total {filterTasks.length} tareas | {cantTasks} pendientes
+            que resolver
+          </p>
+        ) : (
+          <p>No tenes tareas</p>
+        )}
+        {filterTasks.map((t) => {
+          return (
+            <Task
+              key={t.id}
+              task={t}
+              onClickHandlerDelete={onClickHandlerDelete}
+              onClickHandlerCheck={onClickHandlerCheck}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <ul>
-                {filterTasks.length > 0 ? <p>Tenes en total {filterTasks.length} tareas | {cantTasks} pendientes que resolver</p> : <p>No tenes tareas</p>}
-                {filterTasks.map(t => {
-                    return <Task key={t.id} task={t} onClickHandlerDelete={onClickHandlerDelete} onClickHandlerCheck={onClickHandlerCheck} />
-                })}
-            </ul>
-        </div>
-
-    )
-}
-
-export default List
+export default List;
